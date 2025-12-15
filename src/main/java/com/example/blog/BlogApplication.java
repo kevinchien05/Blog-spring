cf3960bb28442602eb1c39c96f723732f63c6ce1
kevin <kevinchien24@gmail.com>
2025-12-15 13:46:49 +0700

    adding environment variable for email

--- src/main/java/com/example/blog/BlogApplication.java
+++ src/main/java/com/example/blog/BlogApplication.java
@@ -3,10 +3,17 @@ package com.example.blog;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class BlogApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+        // System.setProperty("OAUTH2_CLIENT_ID", dotenv.get("OAUTH2_CLIENT_ID"));
+        // System.setProperty("OAUTH2_CLIENT_SECRET", dotenv.get("OAUTH2_CLIENT_SECRET"));
+		System.setProperty("MAIL_USERNAME", dotenv.get("MAIL_USERNAME"));
+        System.setProperty("MAIL_PASSWORD", dotenv.get("MAIL_PASSWORD"));
 		SpringApplication.run(BlogApplication.class, args);
 	}
 
